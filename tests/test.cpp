931236ad279c74d5f380e65ac960b5b4f1e8df45
sarahkimmieee <itssarahkim@gmail.com>
2022-04-28 18:57:33 -0700

    added test cases

--- tests/test.cpp
+++ tests/test.cpp
@@ -3,6 +3,22 @@
 #include "gtest/gtest.h"
 
 
+TEST(PerimeterTest, TwobyFour){
+    Rectangle test(2,4);
+    int val = test.perimeter();
+    EXPECT_EQ(12,val);
+}
+
+TEST(PerimeterTest, ZerobyZero){
+    Rectangle test(0,0);
+    int val = test.perimeter();
+    EXPECT_EQ(0,val);
+}
+TEST(PerimeterTest, OneByOne){
+    Rectangle test(1,1);
+    int val = test.perimeter();
+    EXPECT_EQ(4,val);
+}
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
